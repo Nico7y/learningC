c6ff149cdeb32885cf39b493721505262aa18804
Nicoly Novais <146045310+Nico7y@users.noreply.github.com>
2025-09-19 09:59:15 -0300

    Add files via upload

---
+++ soma.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int numero1, numero2, soma;
+    printf("Digite o primeiro numero: ");
+    scanf("%d", &numero1);
+    printf("Digite o segundo numero: ");
+    scanf("%d", &numero2);
+    soma = numero1 + numero2;
+    printf("A soma de %d + %d = %d\n", numero1, numero2, soma);
+
+    return 0;
+}
